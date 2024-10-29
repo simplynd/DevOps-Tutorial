28c80ebdf6e1d52b4b77cc186f3ea5adb92e8568
Nakul Detroja <N.detroja@gmail.com>
2024-10-29 03:49:46 -0400

    did a small code change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my amazing website test.
+          CI CD is Amazing. Saves lot of time.
         </p>
       </header>
     </div>
